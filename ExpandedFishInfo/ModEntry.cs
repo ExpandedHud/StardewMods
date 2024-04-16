14b9fd4ee5db39515d9602f5790e68fb4536d671
Drew Hoener <drewhoener@gmail.com>
2024-04-16 17:43:43 -0400

    Initial Commit

---
+++ ExpandedFishInfo/ModEntry.cs
@@ -0,0 +1,11 @@
+﻿using StardewModdingAPI;
+
+namespace ExpandedFishInfo;
+
+public class ModEntry : Mod
+{
+  public override void Entry(IModHelper helper)
+  {
+    throw new NotImplementedException();
+  }
+}
